6c294eacdc6227ad299e5eeccb1bdefa419f6cf4
ArinPandey01 <arinpandey01@gmail.com>
2025-03-02 12:18:50 +0530

    feat: add database

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express=require('express');
-const app=express();
 const routes=require('./routes/taskRoutes');
+const connectDB = require('./db/connection');
+require('dotenv').config();
+const app=express();
 app.use(express.json());
 app.use('/task/v1/',routes);
 
-app.listen(3000,()=>{
-    console.log('server listening on port 3000....');
-})
+(()=>{
+    try{
+        connectDB(process.env.DB_URL);
+        app.listen(3000,()=>{
+            console.log('server listening on port 3000....');
+        })
+    }catch(err){
+        console.log(`Error has occured:\n${err}`)
+    }
+})();
